8a3709dbbe8d163cee0b2a584ad1c2d021c20685
vsakkas <vasileios.sakkas96@gmail.com>
2023-03-28 20:59:03 +0300

    Add additional headers

--- sydney/sydney.py
+++ sydney/sydney.py
@@ -5,8 +5,11 @@ import websockets.client as websockets
 from aiohttp import ClientSession
 
 HEADERS = {
-    # Microsoft Edge User Agent.
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.54"
+    "Accept": "application/json",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.54",
 }
 
 BING_CREATE_CONVESATION_URL = "https://www.bing.com/turing/conversation/create"
