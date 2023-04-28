1fa4a14912665e3967a36ed7cf8e2961d6cf62bf
vsakkas <vasileios.sakkas96@gmail.com>
2023-04-28 17:22:42 +0300

    Update user agent version (#40)
    - Update user agent to latest version.

--- sydney/constants.py
+++ sydney/constants.py
@@ -3,7 +3,7 @@ HEADERS = {
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
     "Content-Type": "application/json",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.48",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.64",
 }
 
 BING_CREATE_CONVESATION_URL = "https://www.bing.com/turing/conversation/create"
