a0a069620c2d16cc49c4231da142c45eca4ee0d0
vsakkas <vasileios.sakkas96@gmail.com>
2023-04-28 16:58:10 +0300

    Add tests (#38)
    - Add tests for `ask` method and test both streaming and non-streaming
    versions. Test all available conversation styles.

---
+++ tests/test_ask.py
@@ -0,0 +1,115 @@
+import pytest
+
+from sydney import SydneyClient
+from thefuzz import fuzz
+
+
+@pytest.mark.asyncio
+async def test_ask_precise() -> bool:
+    expected_responses = [
+        "Hello! This is Bing. How can I help you today? 😊",
+        "Hello! How can I help you today? 😊",
+    ]
+
+    async with SydneyClient(style="precise") as sydney:
+        response = await sydney.ask("Hello, Bing!")
+
+        score = 0
+        for expected_response in expected_responses:
+            score = fuzz.token_sort_ratio(response, expected_response)
+            if score >= 80:
+                return True
+
+        assert False, f"Unexpected response: {response}, match score: {score}"
+
+
+@pytest.mark.asyncio
+async def test_ask_balanced() -> bool:
+    expected_responses = ["Hello! How can I help you today? 😊"]
+
+    async with SydneyClient(style="balanced") as sydney:
+        response = await sydney.ask("Hello, Bing!")
+
+        score = 0
+        for expected_response in expected_responses:
+            score = fuzz.token_sort_ratio(response, expected_response)
+            if score >= 80:
+                return True
+
+        assert False, f"Unexpected response: {response}, match score: {score}"
+
+
+@pytest.mark.asyncio
+async def test_ask_creative() -> bool:
+    expected_responses = [
+        "Hello, this is Bing. How can I help? 😊",
+        "Hello, this is Bing. Nice to meet you! 😊",
+    ]
+
+    async with SydneyClient(style="creative") as sydney:
+        response = await sydney.ask("Hello, Bing!")
+
+        score = 0
+        for expected_response in expected_responses:
+            score = fuzz.token_sort_ratio(response, expected_response)
+            if score >= 80:
+                return True
+
+        assert False, f"Unexpected response: {response}, match score: {score}"
+
+
+@pytest.mark.asyncio
+async def test_ask_stream_precise() -> bool:
+    expected_responses = ["Hello! This is Bing. How can I help you today? 😊"]
+
+    async with SydneyClient(style="precise") as sydney:
+        response = ""
+        async for response_token in sydney.ask_stream("Hello, Bing!"):
+            response += response_token
+
+        score = 0
+        for expected_response in expected_responses:
+            score = fuzz.token_sort_ratio(response, expected_response)
+            if score >= 80:
+                return True
+
+        assert False, f"Unexpected response: {response}, match score: {score}"
+
+
+@pytest.mark.asyncio
+async def test_ask_stream_balanced() -> bool:
+    expected_responses = ["Hello! How can I help you today? 😊"]
+
+    async with SydneyClient(style="balanced") as sydney:
+        response = ""
+        async for response_token in sydney.ask_stream("Hello, Bing!"):
+            response += response_token
+
+        score = 0
+        for expected_response in expected_responses:
+            score = fuzz.token_sort_ratio(response, expected_response)
+            if score >= 80:
+                return True
+
+        assert False, f"Unexpected response: {response}, match score: {score}"
+
+
+@pytest.mark.asyncio
+async def test_ask_stream_creative() -> bool:
+    expected_responses = [
+        "Hello, this is Bing. How can I help? 😊",
+        "Hello, this is Bing. Nice to meet you! 😊",
+    ]
+
+    async with SydneyClient(style="creative") as sydney:
+        response = ""
+        async for response_token in sydney.ask_stream("Hello, Bing!"):
+            response += response_token
+
+        score = 0
+        for expected_response in expected_responses:
+            score = fuzz.token_sort_ratio(response, expected_response)
+            if score >= 80:
+                return True
+
+        assert False, f"Unexpected response: {response}, match score: {score}"
