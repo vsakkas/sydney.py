7b6a5815b51b480923cd6b27ec19ad4bba3f8532
vsakkas <vasileios.sakkas96@gmail.com>
2023-08-28 21:44:26 +0300

    Fix typo in constant (#48)
    - Fix minor typo in constant variable

--- sydney/constants.py
+++ sydney/constants.py
@@ -6,7 +6,7 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.62",
 }
 
-BING_CREATE_CONVESATION_URL = "https://www.bing.com/turing/conversation/create"
+BING_CREATE_CONVERSATION_URL = "https://www.bing.com/turing/conversation/create"
 BING_CHATHUB_URL = "wss://sydney.bing.com/sydney/ChatHub"
 
 DELIMETER = "\x1e"  # Record separator character.
