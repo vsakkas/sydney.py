75295c857688b42f70a24b12c1c6cc833cb6ec9a
vsakkas <vasileios.sakkas96@gmail.com>
2023-10-07 00:51:04 +0300

    Fix create conversation URL (#59)
    - Fix create conversation URL by replacing the old one which is becoming
    inaccessible with an up to date URL that currently works

--- sydney/constants.py
+++ sydney/constants.py
@@ -6,7 +6,7 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.47",
 }
 
-BING_CREATE_CONVERSATION_URL = "https://www.bing.com/turing/conversation/create"
+BING_CREATE_CONVERSATION_URL = "https://edgeservices.bing.com/edgesvc/turing/conversation/create"
 BING_GET_CONVERSATIONS_URL = "https://www.bing.com/turing/conversation/chats"
 BING_CHATHUB_URL = "wss://sydney.bing.com/sydney/ChatHub"
 
