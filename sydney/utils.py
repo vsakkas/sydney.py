77169d1d0a90d3871aabc041854c74669e442ed8
vsakkas <vasileios.sakkas96@gmail.com>
2023-11-18 17:21:57 +0200

    Add cookie conversion function

--- sydney/utils.py
+++ sydney/utils.py
@@ -8,3 +8,13 @@ def as_json(message: dict) -> str:
     Convert message to JSON, append delimeter character at the end.
     """
     return json.dumps(message) + DELIMETER
+
+
+def cookies_as_dict(cookies: str) -> dict:
+    """
+    Convert a string of cookies into a dictionary.
+    """
+    return {
+        key_value.split("=")[0]: key_value.split("=")[1]
+        for key_value in cookies.split("; ")
+    }
