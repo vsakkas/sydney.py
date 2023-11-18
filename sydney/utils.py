57d23e199a32959bb16289ccd917372054e6ba17
vsakkas <vasileios.sakkas96@gmail.com>
2023-11-18 18:12:29 +0200

    Fix cookie util function

--- sydney/utils.py
+++ sydney/utils.py
@@ -15,6 +15,6 @@ def cookies_as_dict(cookies: str) -> dict:
     Convert a string of cookies into a dictionary.
     """
     return {
-        key_value.split("=")[0]: key_value.split("=")[1]
+        key_value.split("=")[0]: "=".join(key_value.split("=")[1:])
         for key_value in cookies.split("; ")
     }
