4a275d39daef9c163ac3ed9d5e69db629daafe78
vsakkas <vasileios.sakkas96@gmail.com>
2024-05-11 13:30:50 +0200

    Improve API compatibility (#169)
    - Update bundle version, headers and user agent
    - Improve compatibility with Copilot API by updating the request
    parameters

--- sydney/constants.py
+++ sydney/constants.py
@@ -1,11 +1,11 @@
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.2277.113"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.2478.97"
 
 CREATE_HEADERS = {
     "Accept": "application/json",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9",
     "Referer": "https://copilot.microsoft.com/",
-    "Sec-Ch-Ua": '"Microsoft Edge";v="121", "Chromium";v="121", "Not?A_Brand";v="8"',
+    "Sec-Ch-Ua": '"Microsoft Edge";v="124", "Chromium";v="124", "Not?A_Brand";v="8"',
     "Sec-Ch-Ua-Mobile": "?0",
     "Sec-Ch-Ua-Platform": "Windows",
     "Sec-Fetch-Dest": "empty",
@@ -31,7 +31,7 @@ KBLOB_HEADERS = {
     "Accept-Language": "en-US,en;q=0.9",
     "Content-Type": "multipart/form-data",
     "Referer": "https://copilot.microsoft.com/",
-    "Sec-Ch-Ua": '"Microsoft Edge";v="121", "Chromium";v="121", "Not?A_Brand";v="8"',
+    "Sec-Ch-Ua": '"Microsoft Edge";v="124", "Chromium";v="124", "Not?A_Brand";v="8"',
     "Sec-Ch-Ua-Mobile": "?0",
     "Sec-Ch-Ua-Platform": "Windows",
     "Sec-Fetch-Dest": "empty",
@@ -41,11 +41,9 @@ KBLOB_HEADERS = {
     "X-Edge-Shopping-Flag": "0",
 }
 
-BUNDLE_VERSION = "1.1573.2"
+BUNDLE_VERSION = "1.1729.0"
 
-BING_CREATE_CONVERSATION_URL = (
-    f"https://copilot.microsoft.com/turing/conversation/create?bundleVersion={BUNDLE_VERSION}"
-)
+BING_CREATE_CONVERSATION_URL = f"https://copilot.microsoft.com/turing/conversation/create?bundleVersion={BUNDLE_VERSION}"
 BING_GET_CONVERSATIONS_URL = "https://copilot.microsoft.com/turing/conversation/chats"
 BING_CHATHUB_URL = "wss://sydney.bing.com/sydney/ChatHub"
 BING_KBLOB_URL = "https://copilot.microsoft.com/images/kblob"
