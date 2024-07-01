39d6f817d27cf2e4bf2eeb58d50914ebbc90c1c6
vsakkas <vasileios.sakkas96@gmail.com>
2024-07-01 21:43:51 +0200

    Update bundle version

--- sydney/constants.py
+++ sydney/constants.py
@@ -41,7 +41,7 @@ KBLOB_HEADERS = {
     "X-Edge-Shopping-Flag": "0",
 }
 
-BUNDLE_VERSION = "1.1729.0"
+BUNDLE_VERSION = "1.1782.0"
 
 BING_CREATE_CONVERSATION_URL = f"https://copilot.microsoft.com/turing/conversation/create?bundleVersion={BUNDLE_VERSION}"
 BING_GET_CONVERSATIONS_URL = "https://copilot.microsoft.com/turing/conversation/chats"
